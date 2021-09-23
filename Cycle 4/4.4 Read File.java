076c85f743abdf9f22fcab8e2facb0b454b1aa8d
sajillidhan123 <76091944+sajillidhan123@users.noreply.github.com>
2021-09-23 19:54:04 +0530

    Create 4.4 Read File.java

---
+++ Cycle 4/4.4 Read File.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class ReadFile {
+  public static void main(String[] args) throws Exception {
+     int i=0;
+
+     FileInputStream fin = new FileInputStream("File.txt");
+     FileOutputStream fodd = new FileOutputStream("Even.txt");
+     FileOutputStream feven = new FileOutputStream("Odd.txt");
+    
+     while((i=fin.read())!=-1) 
+     {
+      if(i%2==0)
+      {
+      feven.write(i);
+     }
+      else
+      fodd.write(i);
+     }
+    
+     fin.close();
+     feven.close();
+     fodd.close();
+
+      }
+
+    } 
